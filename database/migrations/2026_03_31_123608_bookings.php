838826c88515138c1aeb02b5dabf89a4c2c53797
Mariam982440 <mariam1elhaimer@gmail.com>
2026-03-29 20:00:00 +0100

    feat : création de la migration bookings

---
+++ database/migrations/2026_03_31_123608_bookings.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('client_profiles');
+            $table->foreignId('time_slot_id')->constrained();
+            $table->string('subject');
+            $table->text('message')->nullable();
+            $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
