19c6e1c05b169a0710319f15e3557870007dde58
Mariam982440 <mariam1elhaimer@gmail.com>
2026-03-29 20:00:00 +0100

    feat : création de la migration time_slots

---
+++ database/migrations/2026_03_31_123517_time_slots.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('time_slots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('availability_id')->constrained();
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->boolean('is_booked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
