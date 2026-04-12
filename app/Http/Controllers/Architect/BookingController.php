9145dc5446e2a67ca8097f11d6eac289b6972966
Mariam982440 <mariam1elhaimer@gmail.com>
2026-04-12 14:45:25 +0100

    feat: création du controller BookingController

---
+++ app/Http/Controllers/Architect/BookingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Architect;
+
+use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Enums\BookingStatus;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    
+}
