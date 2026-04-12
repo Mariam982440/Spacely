48733fc227c7a803c980dc3c513f9f34a6df2945
Mariam982440 <mariam1elhaimer@gmail.com>
2026-04-12 14:11:18 +0100

    feat: création du controller ProjectController

---
+++ app/Http/Controllers/Architect/ProjectController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Architect;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectController extends Controller
+{
+    //
+}
