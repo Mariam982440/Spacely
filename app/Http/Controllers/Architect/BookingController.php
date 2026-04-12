a3363c33b1aa2b1b6fb6a3ef9312adf3839daecb
Mariam982440 <mariam1elhaimer@gmail.com>
2026-04-12 14:47:28 +0100

    feat : ajout de la methode index dans BookingController

--- app/Http/Controllers/Architect/BookingController.php
+++ app/Http/Controllers/Architect/BookingController.php
@@ -9,5 +9,22 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    
+    public function index(Request $request)
+    {
+        $query = Booking::query()
+            ->whereHas('timeSlot.availability', function ($q) {
+                $q->where('architect_id', auth()->user()->architectProfile->id);
+            })
+            ->with('timeSlot', 'clientProfile.user')
+            ->latest();
+
+        // Filtre par statut
+        if ($request->filled('status')) {
+            $query->where('status', $request->status);
+        }
+
+        $bookings = $query->paginate(10);
+
+        return view('architect.bookings.index', compact('bookings'));
+    }
 }
