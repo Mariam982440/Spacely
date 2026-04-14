313ad05df393eee214992c8175e46cf0b534dd7a
Mariam982440 <mariam1elhaimer@gmail.com>
2026-04-14 21:37:03 +0100

    feat : ajout de la methode index dans QuoteController

--- app/Http/Controllers/Architect/QuoteController.php
+++ app/Http/Controllers/Architect/QuoteController.php
@@ -9,5 +9,16 @@ use Illuminate\Http\Request;
  
 class QuoteController extends Controller
 {
-    //
+    public function index()
+    {
+        $quotes = Quote::whereHas('booking.timeSlot.availability', function ($q) {
+                $q->where('architect_id', auth()->user()->architectProfile->id);
+            })
+            ->with('booking.clientProfile.user', 'items')
+            ->latest()
+            ->paginate(10);
+ 
+        return view('architect.quotes.index', compact('quotes'));
+    }
+    
 }
