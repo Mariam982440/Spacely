5893c8851ce816687d6258ce2f6296cc4fe549f6
Mariam982440 <mariam1elhaimer@gmail.com>
2026-04-16 09:11:46 +0100

    feat : ajout de la methode index dans BlogController

--- app/Http/Controllers/Architect/BlogController.php
+++ app/Http/Controllers/Architect/BlogController.php
@@ -1,10 +1,21 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Architect;
+ 
+use App\Http\Controllers\Controller;
+use App\Models\BlogPost;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = BlogPost::where('architect_id', auth()->user()->architectProfile->id)
+            ->latest()
+            ->paginate(10);
+ 
+        return view('architect.blog.index', compact('posts'));
+    }
+    
 }
