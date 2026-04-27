917764098e6be68b718a3beb758c9b8bb603a053
Mariam982440 <mariam1elhaimer@gmail.com>
2026-04-27 20:22:17 +0100

    feat : ajout de la methode show dans MessageController

--- app/Http/Controllers/Architect/MessageController.php
+++ app/Http/Controllers/Architect/MessageController.php
@@ -31,4 +31,24 @@ class MessageController extends Controller
  
         return view('architect.messages.index', compact('conversations', 'unreadCount'));
     }
+    public function show(User $user)
+    {
+        $myId = auth()->id();
+ 
+        $messages = Message::where(function ($q) use ($myId, $user) {
+                $q->where('sender_id', $myId)->where('receiver_id', $user->id);
+            })
+            ->orWhere(function ($q) use ($myId, $user) {
+                $q->where('sender_id', $user->id)->where('receiver_id', $myId);
+            })
+            ->orderBy('created_at')
+            ->get();
+ 
+        Message::where('sender_id', $user->id)
+            ->where('receiver_id', $myId)
+            ->where('is_read', false)
+            ->update(['is_read' => true]);
+ 
+        return view('architect.messages.show', compact('messages', 'user'));
+    }
 }
